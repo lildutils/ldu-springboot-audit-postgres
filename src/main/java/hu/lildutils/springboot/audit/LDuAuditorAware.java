58b946aa25ad902175e93c40d40d6eb8eb2a2d97
lildworks <lildworks@gmail.com>
2021-10-01 14:57:47 +0200

    IMPR: Removed old project files

--- src/main/java/hu/lildutils/springboot/audit/LDuAuditorAware.java
+++
@@ -1,9 +0,0 @@
-package hu.lildutils.springboot.audit;
-
-import java.io.Serializable;
-
-import org.springframework.data.domain.AuditorAware;
-
-public interface LDuAuditorAware<T extends Serializable> extends AuditorAware<T> {
-
-}
